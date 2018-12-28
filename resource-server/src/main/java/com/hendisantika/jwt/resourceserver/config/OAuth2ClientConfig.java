852bc928009b0e05290fa94273312256c2d9d41b
Hendi Santika <hendisantika@gmail.com>
2018-12-28 07:05:25 +0700

    #19 - Add OAuth2 Client Config in resource-server

---
+++ resource-server/src/main/java/com/hendisantika/jwt/resourceserver/config/OAuth2ClientConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.jwt.resourceserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt-oauth2-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-28
+ * Time: 07:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableOAuth2Client
+public class OAuth2ClientConfig {
+}
