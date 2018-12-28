a79da7982d8dbd64c6384407f8089ef2b0782676
Hendi Santika <hendisantika@gmail.com>
2018-12-28 07:19:24 +0700

    #30 - Add Security Config in auth-server

---
+++ auth-server/src/main/java/com/hendisantika/jwt/authserver/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.hendisantika.jwt.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt-oauth2-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-28
+ * Time: 07:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("testpass").roles("USER");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean()
+            throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
